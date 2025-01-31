00116d677d2e3f37559953418d6ecbc68a3235b8
Tapan48 <20BDS059@iiitdwd.ac.in>
2025-01-31 15:33:49 +0530

    Implemented useExpenses hook for state management

---
+++ src/hooks/useExpenses.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+export interface Expense {
+  id: string;
+  description: string;
+  amount: number;
+  date: Date;
+}
+
+export const useExpenses = () => {
+  const [expenses, setExpenses] = useState<Expense[]>([]);
+
+  useEffect(() => {
+    const storedExpenses = localStorage.getItem("expenses");
+    if (storedExpenses) {
+      setExpenses(JSON.parse(storedExpenses));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+
+  const addExpense = (expense: Omit<Expense, "id">) => {
+    setExpenses([...expenses, { ...expense, id: uuidv4() }]);
+  };
+
+  const removeExpense = (id: string) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
+
+  return { expenses, addExpense, removeExpense };
+};
